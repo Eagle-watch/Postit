9b15c29a5359b7698a9d18de924ae47a045c3c08
Francois Bryan <b.francois.gj@gmail.com>
2021-11-17 14:49:49 +0100

    Final

--- index.js
+++ index.js
@@ -1,15 +1,101 @@
 let tablepostit = []
+let numpostitselect = -1
+let action= ""
+let ecrirepostit = -1
+
+
 document.getElementById("red").addEventListener("click",()=>{
-    tablepostit.push(new Postit(600,400,300,300,"red","coucou",tablepostit.length))
+    tablepostit.push(new Postit(600,400,300,300,"red","Scorpions",tablepostit.length))
     tablepostit[tablepostit.length-1].affichePostit()
 })
+
 document.getElementById("blue").addEventListener("click",()=>{
-    tablepostit.push(new Postit(800,300,300,300,"blue","coucou",tablepostit.length))
+    tablepostit.push(new Postit(800,300,300,300,"blue","KISS",tablepostit.length))
     tablepostit[tablepostit.length-1].affichePostit()
 })
+
 document.getElementById("yellow").addEventListener("click",()=>{
-    tablepostit.push(new Postit(1000,400,300,300,"yellow","coucou",tablepostit.length))
+    tablepostit.push(new Postit(1000,400,300,300,"yellow","Led Zeppelin",tablepostit.length))
     tablepostit[tablepostit.length-1].affichePostit()
 })
+// fonction deplacer
+
+document.body.addEventListener("mousemove",(event)=>{
+    let x = event.clientX
+    let y = event.clientY
+    if (numpostitselect>-1 && action=="deplace"){
+        tablepostit[numpostitselect].deplacement(x,y)
+        tablepostit[numpostitselect].affichePostit()
+    }
+    if (numpostitselect>-1 && action=="redim"){
+        tablepostit[numpostitselect].redimensionement(event.clientX-tablepostit[numpostitselect].posX+tablepostit[numpostitselect].larg,event.clientY-tablepostit[numpostitselect].posY+tablepostit[numpostitselect].haut)
+        tablepostit[numpostitselect].affichePostit()
+    }
+
+})
+
+document.body.addEventListener("click",(event)=>{
+    numpostitselect=-1
+    action=""
+})
+// Fonction ecrire
+
+document.body.addEventListener("keydown",(ecris)=>{
+    if (ecrirepostit>-1 && action =="ecrire") {
+        // Touche effacé 
+        if(ecris.key=="Backspace"){
+            tablepostit[ecrirepostit].changertexte(tablepostit[ecrirepostit].texte.substr(0,tablepostit[ecrirepostit].texte.length-1))
+            tablepostit[ecrirepostit].affichePostit()
+        }
+        //Touche Majuscule
+        else if (ecris.key=="Shift"){
+          
+        }
+        //Touche Entrer
+        else if (ecris.key=="Enter"){
+            tablepostit[ecrirepostit].changertexte(tablepostit[ecrirepostit].texte+"<br>")
+            tablepostit[ecrirepostit].affichePostit()
+        }
+        //  Touche Clé (ou Caps lock)
+        else if (ecris.key=="CapsLock"){
+            
+        }
+        //Touche CTRL 
+        else if (ecris.key=="Control"){
+            
+        }
+        // Touche Alt
+        else if (ecris.key=="Alt"){
+        
+        }
+        //Touche Tab
+        else if (ecris.key=="Tab"){
+            tablepostit[ecrirepostit].changertexte(tablepostit[ecrirepostit].texte+" ")
+            tablepostit[ecrirepostit].affichePostit()
+        }
+        //Touche Accent circonflexe
+        else if (ecris.key=="Dead"){
+            tablepostit[ecrirepostit].changertexte(tablepostit[ecrirepostit].ecris.key+"^")
+            tablepostit[ecrirepostit].affichePostit()
+        }
+        else{
+            tablepostit[ecrirepostit].changertexte(tablepostit[ecrirepostit].texte+ecris.key)
+            tablepostit[ecrirepostit].affichePostit()
+        }
+    }
+})
+
+/**
+ * Fonction qui supprime un Postit
+ * 
+ * @param {number} numpostit numero du Postit a supprimer
+ */
+function supprimer(numpostit)
+{
+tablepostit.splice(numpostit,1)
+}
+
+
+
 
 
--- postit.js
+++ postit.js
@@ -7,7 +7,16 @@ class Postit{
     texte
     supprimer
     id
-
+/**
+ * C'est la fonction de construction de postit
+ * @param {number} x  est la position sur mon absycce (Horizontale)
+ * @param {number} y est la position sur l'ordonée (Verticale)
+ * @param {number} largeur largeur en px du postit
+ * @param {number} hauteur hauteur du postit en px
+ * @param {string} couleur la couleur choise red = rouge , blue = bleu , yellow = jaune
+ * @param {string} texte le texte que l'on ecris sur le Postit
+ * @param {number} id Numero du Postit
+ */
     constructor(x,y,largeur,hauteur,couleur,texte,id){
         this.x=x
         this.y=y
@@ -17,24 +26,41 @@ class Postit{
         this.texte=texte
         this.id=id
     }
-    supprimer(){
-        this.supprimer
-    }
+    /**
+     * Elle deplace le Postit
+     * @param {number} x est la position sur mon absycce (Horizontale)
+     * @param {number} y est la position sur l'ordonée (Verticale)
+     */
     deplacement(x,y){
         this.x=x
-        this.y=y
-       
+        this.y=y  
     }
+    /**
+     * Elle redimensionne un Postit
+     * @param {number} largeur largeur en px du postit
+     * @param {number} hauteur hauteur du postit en px
+     */
     redimensionement(largeur, hauteur) {
         this.largeur=largeur
         this.hauteur=hauteur
     }
+    /**
+     * Pour Changer le texte
+     * @param {string} texte le texte que l'on ecris sur le Postit
+     */
     changertexte(texte){
         this.texte=texte
     }
+    /**
+     * Sert as changer la couleur
+     * @param {string} couleur la couleur choise red = rouge , blue = bleu , yellow = jaune
+     */
     changercouleur(couleur){
         this.couleur=couleur
     }
+    /**
+     * Elle Affiche le Postit
+     */
     affichePostit(){
         let postit= document.getElementById("postit"+ this.id)
         if(postit== null){
@@ -50,48 +76,73 @@ class Postit{
         postit.style.height=this.hauteur+"px"
         postit.innerHTML=this.texte
         postit.className = "Ombre"
-        postit.style.transform = "rotate(0deg)"
-        setTimeout(()=>{
-            postit.style.transform = "rotate(360deg)"
-        },200)
+        //postit.style.transform = "rotate(0deg)"
+        //setTimeout(()=>{
+        //    postit.style.transform = "rotate(360deg)"
+        //},200)
         let menu = document.createElement("div")
         postit.appendChild(menu)
         menu.className = "bas"
-
+        
         let bouton1= document.createElement("div")
         menu.appendChild(bouton1)
         bouton1.className = "fa-solid fa-pencil"    //Ecris
-        bouton1.addEventListener("click",()=> 
-        {this.changertexte(),this.affichePostit()})
+        bouton1.addEventListener("click",(event)=> 
+        {
+            action="ecrire"
+            ecrirepostit=this.id
+            event.stopPropagation()
+        })
         
 
         let bouton2= document.createElement("div")
         menu.appendChild(bouton2)
         bouton2.className = "fa-solid fa-down-left-and-up-right-to-center"  // Redimensionner
-        bouton2.addEventListener("click",()=> {
-        this.redimensionement(),this.affichePostit()})
+        bouton2.addEventListener("click",(event)=>{
+            this.posX = event.clientX
+            this.posY = event.clientY
+            this.larg= this.x
+            this.haut= this.y
+            numpostitselect= this.id
+        action="redim"
+        event.stopPropagation()
+
+           
+        })
         
     
         let bouton3= document.createElement("div")
         menu.appendChild(bouton3)
         bouton3.className = "fa-solid fa-up-down-left-right"    // Deplacer
-        bouton3.addEventListener("click",()=>{
-            console.log("test")
-            this.deplacement(500,500)
-            this.affichePostit()
+        bouton3.addEventListener("click",(event)=>{
+            action="deplace"
+          numpostitselect= this.id
+          event.stopPropagation()
         })
 
-        let bouton4= document.createElement("div")
+        let bouton4 = document.createElement("div")
         menu.appendChild(bouton4)
-        bouton4.className = "fa-solid fa-trash-can"     // Suprimer
-        bouton4.addEventListener("click",this.supprimer)
-        //this.affichePostit()
-
-        let bouton5 = document.createElement("div")
+        bouton4.className = "fa-solid fa-droplet"    //Change couleur 
+        bouton4.addEventListener("click", ()=>{
+            if (this.couleur=="red") {
+                this.couleur= "blue"
+            }
+            else if(this.couleur=="blue"){
+                this.couleur="yellow"
+            }
+            else {
+                this.couleur="red"
+            }
+            this.affichePostit()
+        })
+        
+        let bouton5= document.createElement("div")
         menu.appendChild(bouton5)
-        bouton5.className = "fa-solid fa-droplet"    //Change couleur 
-        bouton5.addEventListener("click", ()=>{
-            this.changercouleur("red","blue","yellow")
-            ,this.affichePostit()})
-        }
+        bouton5.className = "fa-solid fa-trash-can"     // Suprimer
+        bouton5.addEventListener("click",(event) =>{
+        postit.parentNode.removeChild(postit)
+        supprimer(this.id)
+        })
     }
+}
+
