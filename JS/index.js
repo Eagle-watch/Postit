2a0f7027accdb960a1546f6464a3ea06619ec1b3
Francois Bryan <b.francois.gj@gmail.com>
2021-11-18 16:08:00 +0100

    V1

--- JS/index.js
+++ JS/index.js
@@ -95,7 +95,57 @@ function supprimer(numpostit)
 tablepostit.splice(numpostit,1)
 }
 
+/**
+ * 
+ * @param {*} name 
+ * @param {*} value 
+ * @param {*} days 
+ */
+
+function createCookie(name,value,days) {
+    let expires
+	if (days) {
+		let date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		 expires = "; expires="+date.toGMTString();
+	}
+	else {
+         expires = "";
+    }
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	let nameEQ = name + "=";
+	let ca = document.cookie.split(';');
+	for(let i=0;i < ca.length;i++) {
+		let c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+setInterval(() => {
+    let test = JSON.stringify(tablepostit)
+    createCookie("teste",test,365)
+}, 1000);
+
+                ///////////////////////////////////////////////////////////////////////
 
 
-
+window.addEventListener("load",()=>{
+    let cookie = JSON.parse(readCookie("teste"))
+   // tablepostit.push()
+   for(let i=0;i<cookie.length;i++){
+    tablepostit.push(new Postit(cookie[i].x,cookie[i].y,cookie[i].largeur,cookie[i].hauteur,cookie[i].couleur,cookie[i].texte,tablepostit.length))
+    tablepostit[tablepostit.length-1].affichePostit()
+}
+   console.log(cookie)
+  
+})
 
